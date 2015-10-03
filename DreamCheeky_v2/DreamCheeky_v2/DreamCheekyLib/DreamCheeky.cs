52e22fd2016acb29adcf0016ed7e8095f3a9cdf5
chriswmercer <chriswm@me.com>
2015-10-03 10:56:11 +0100

    Initial Commit - from local copy
    From local copy

---
+++ DreamCheeky_v2/DreamCheeky_v2/DreamCheekyLib/DreamCheeky.cs
@@ -0,0 +1,29 @@
+﻿using DreamCheeky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamCheekyLib
+{
+    public class DreamCheeky : IDisposable
+    {
+        private BigRedButton bigRedButton;
+
+        public DreamCheeky(EventHandler lidClosed, EventHandler lidOpen, EventHandler buttonPressed)
+        {
+            bigRedButton = new BigRedButton();
+            bigRedButton.LidClosed += lidClosed;
+            bigRedButton.LidOpen += lidOpen;
+            bigRedButton.ButtonPressed += buttonPressed;
+            bigRedButton.Start();
+        }
+
+        public void Dispose()
+        {
+            bigRedButton.Stop();
+            bigRedButton = null;
+        }
+    }
+}
---
+++ DreamCheeky_v2/DreamCheeky_v2/DreamCheeky_v2/frmMAin.cs
@@ -0,0 +1,38 @@
+﻿using DreamCheekyLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DreamCheeky_v2
+{
+    public partial class frmMain : Form
+    {
+        private void LidClosed(object sender, EventArgs e)
+        {
+            Console.WriteLine("CLOSED");
+        }
+
+        private void LidOpen(object sender, EventArgs e)
+        {
+            Console.WriteLine("OPEN");
+        }
+
+        private void ButtonPressed(object sender, EventArgs e)
+        {
+            Console.WriteLine("PRESSED");
+        }
+
+        public frmMain()
+        {
+            InitializeComponent();
+            DreamCheeky brb = new DreamCheeky(new EventHandler(this.LidClosed), new EventHandler(this.LidOpen), new EventHandler(this.ButtonPressed));
+            }
+        }
+    }
+}
